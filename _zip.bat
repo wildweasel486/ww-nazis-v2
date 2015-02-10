5d899654e62bbba14208566036aa900ba0e26599
wildweasel486 <wild.weasel@gmail.com>
2015-02-09 21:14:21 -0800

    batch file now outputs datestamped pk3

--- _zip.bat
+++ _zip.bat
@@ -1,7 +1,14 @@
 @echo off
 color F
 
-set NAME=ww-doomnukem
+SET HOUR=%time:~0,2%
+SET dtStamp9=%date:~-4%%date:~4,2%%date:~7,2%_0%time:~1,1%%time:~3,2%%time:~6,2% 
+SET dtStamp24=%date:~-4%%date:~4,2%%date:~7,2%_%time:~0,2%%time:~3,2%%time:~6,2%
+
+if "%HOUR:~0,1%" == " " (SET dtStamp=%dtStamp9%) else (SET dtStamp=%dtStamp24%)
+
+ECHO %dtStamp%
+set NAME=ww-nazis-dev-%dtStamp%
 set /p TYPE="Would you like to build a pk3 or pk7? (default pk3): " % = %
 
 cd pk3
